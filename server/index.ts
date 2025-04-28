1541722c6dfae0b275a572d6802c5c21ec8d1e3e
Rick <rick-ame@outlook.com>
2025-04-29 00:32:38 +0800

    express serve static files

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,7 @@
 import 'dotenv/config'
 
+import path from 'node:path'
+
 import express from 'express'
 import { AUTH, PREFIX } from 'shared/apis'
 
@@ -9,6 +11,9 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
+const staticFiles = path.resolve(__dirname, '../../../client/dist')
+app.use(express.static(staticFiles))
+
 app.use(express.json())
 
 app.post(`${PREFIX}${AUTH.signUp}`, (req, res) => {
@@ -16,6 +21,10 @@ app.post(`${PREFIX}${AUTH.signUp}`, (req, res) => {
   res.status(200).json({
     message: `Hello ${username}!`,
   })
+})
+
+app.get('/*splat', (req, res) => {
+  res.sendFile(path.join(staticFiles, 'index.html'))
 })
 
 app.listen(PORT, () => {
